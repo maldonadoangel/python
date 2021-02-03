80af9a62d4b19eca2e2febea2ffa15018b1d7ec9
Angel Morales <maldonadoangel7069@gmail.com>
2021-02-02 18:56:36 -0600

    operadores

---
+++ 02.operadores.py
@@ -0,0 +1,14 @@
+print(4+2)
+print(4-2)
+#imprime el valor de 6 ps el menos convierte el dos negativo a positivo
+print(4 - -2)
+#multiplicacion
+print(4*2)
+#Elevar un numero, exponente
+print(4**2)
+#Devuelve el resultado de valor flotante
+print(6/2)
+#devuelve el valor entero ya que ese valor lo encerramos en un metodo entero
+print(int(6/2))
+#residuo
+print(6%2)
