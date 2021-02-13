7b0ba543406def2c5bbad6e98cb3e2181750b4d5
Angel Morales <maldonadoangel7069@gmail.com>
2021-02-13 05:22:28 -0600

    Modulos (funciones)

---
+++ modulos/modulos_aritmetica.py
@@ -0,0 +1,14 @@
+def sumar(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if(num2 == 0):
+        return num2 / num1
+    else:
+        return num1 / num2
---
+++ test_aritmetica.py
@@ -0,0 +1,7 @@
+from modulos import modulos_aritmetica as aritmetica 
+
+resultado = aritmetica.sumar(1, 5)
+print(resultado)
+resultado = aritmetica.division(1, 0)
+print(resultado)
+
