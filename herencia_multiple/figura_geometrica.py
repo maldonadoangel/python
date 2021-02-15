e438b11b93888520938bc4efc6bb42dd5949c0bf
Angel Morales <maldonadoangel7069@gmail.com>
2021-02-15 03:08:08 -0600

    Clases abstractas

--- herencia_multiple/figura_geometrica.py
+++ herencia_multiple/figura_geometrica.py
@@ -1,4 +1,6 @@
-class FiguraGeometrica:
+#ABC = ABSTRACT BASE CLASS
+from abc import ABC, abstractmethod
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
         self.__ancho = ancho
         self.__alto = alto
@@ -12,5 +14,10 @@ class FiguraGeometrica:
         return self.__alto
     def set_alto(self, alto):
         self.__alto = alto
+    
+    @abstractmethod
+    def area(self):
+        pass
+    
     
     
--- herencia_multiple/test_figura_geometrica.py
+++ herencia_multiple/test_figura_geometrica.py
@@ -1,5 +1,9 @@
+from figura_geometrica import FiguraGeometrica
 from cuadrado import Cuadrado
 
+
+#No es posible crear objetos de una clase abstracta
+# figurageo = FiguraGeometrica()
 
 cuadrado = Cuadrado(5, "Azul")
 print(cuadrado.area())
