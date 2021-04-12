55378c2d51aaea2f8b4398764f7efaa1010202ef
Angel Morales <maldonadoangel7069@gmail.com>
2021-04-12 00:20:10 -0600

    Programa de fahrenheit a celsius

---
+++ fahrenheit_a_celcius.py
@@ -0,0 +1,8 @@
+#Programa que convierta de farenheit a celcius
+
+temperaturaFarenheit = float(input('Ingrese la temperatura en Farenheit: '))
+
+temperaturaCelsius = (temperaturaFarenheit - 32)* 5/9
+
+print('La temperatura de Farenheit a Celsius es: ', temperaturaCelsius)
+
---
+++ primer_programa.py
@@ -0,0 +1,6 @@
+numero1 = input('Ingresa un numero entero: ')
+numero2 = input('Ingresa otro numero entero: ')
+numero3 = input('Ingresa un ultimo numero: ')
+
+print('El numero mayor es: ',max(numero1,numero2,numero3))
+print('El numero menor es: ',min(numero1,numero2,numero3))
